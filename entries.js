7a7af84160382253b4d9916b37641b43a62adecb
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-11-05 14:45:58 +0000

    object methods of javascript

---
+++ entries.js
@@ -0,0 +1,12 @@
+// 1. Object.entries() method
+/*
+Object.entries() in javascript is a built in method that returns an array of key - value pairs
+from an object
+ */
+
+let person ={
+    fname : "Syed",
+    age : 20,
+    isAlive : true,
+};
+console.log(Object.entries(person));
